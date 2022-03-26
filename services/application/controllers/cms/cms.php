2c7efb703ad41b52a47172f7c0125d2ad0dce716
Bharani Palani <barani.potshot@gmail.com>
2022-03-26 19:12:43 +0530

    controller updated

--- services/application/controllers/cms/cms.php
+++ services/application/controllers/cms/cms.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-class home extends CI_Controller
+class cms extends CI_Controller
 {
     public function __construct()
     {
