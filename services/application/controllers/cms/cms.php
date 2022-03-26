4fe20ec7d13f1f6d4d85c752c2e0d24f00189d83
Bharani Palani <barani.potshot@gmail.com>
2022-03-26 19:29:08 +0530

    case added

--- services/application/controllers/cms/cms.php
+++ services/application/controllers/cms/cms.php
@@ -1,16 +1,16 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-class cms extends CI_Controller
+class Cms extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('cms_model');
+        $this->load->model('Cms_model');
         $this->load->library('../controllers/auth');
     }
     public function getPages()
     {
-        $data['response'] = $this->cms_model->getPages();
+        $data['response'] = $this->Cms_model->getPages();
         $this->auth->response($data, [], 200);
     }
 }
--- services/application/models/cms_model.php
+++ services/application/models/cms_model.php
@@ -1,6 +1,6 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class cms_model extends CI_Model
+class Cms_model extends CI_Model
 {
     public function __construct()
     {
