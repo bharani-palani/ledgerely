bc3b759912444d9b81a970d770fee3e8a68fbf39
Bharani Palani <barani.potshot@gmail.com>
2022-03-27 16:18:43 +0530

    test 12

--- services/application/controllers/cms/cms.php
+++ services/application/controllers/cms/cms.php
@@ -10,28 +10,28 @@ class cms extends CI_Controller
     }
     public function getPages()
     {
-        // $validate = $this->auth->validateAll();
-		// if ($validate === 2) {
-		// 	$this->auth->invalidTokenResponse();
-		// }
-		// if ($validate === 3) {
-		// 	$this->auth->invalidDomainResponse();
-		// }
-		// if ($validate === 1) {
+        $validate = $this->auth->validateAll();
+		if ($validate === 2) {
+			$this->auth->invalidTokenResponse();
+		}
+		if ($validate === 3) {
+			$this->auth->invalidDomainResponse();
+		}
+		if ($validate === 1) {
             $data = $this->cms_model->getPages();
             $newData = array();
             for($i=0; $i<count($data); $i++) {
                 for($j=0; $j<count($data[$i]); $j++) {
-                    $newData[$j]['page_id'] = $data[$i]['page_id'];
-                    $newData[$j]['page_label'] = $data[$i]['page_label'];
-                    $newData[$j]['page_route'] = $data[$i]['page_route'];
-                    $newData[$j]['page_object'] = json_decode($data[$i]['page_object']);
-                    $newData[$j]['hasAccessTo'] = json_decode(json_encode(explode(",",$data[$i]['hasAccessTo'])));
+                    $newData[$i]['page_id'] = $data[$i]['page_id'];
+                    $newData[$i]['page_label'] = $data[$i]['page_label'];
+                    $newData[$i]['page_route'] = $data[$i]['page_route'];
+                    $newData[$i]['page_object'] = json_decode($data[$i]['page_object']);
+                    $newData[$i]['hasAccessTo'] = json_decode(json_encode(explode(",",$data[$i]['hasAccessTo'])));
                 }
             }
-            print_r($newData);
-        //     $newData["response"] = $newData;
-        //     $this->auth->response($newData, [], 200);
-        // }
+            $newData["response"] = $newData;
+            // print_r($newData);
+            $this->auth->response($newData, [], 200);
+        }
     }
 }
