2149700feb712a5e9a76f9984f1e7c9e8645bca0
Bharani Palani <barani.potshot@gmail.com>
2022-04-05 00:40:44 +0530

    test2

--- services/application/controllers/cms/cms.php
+++ services/application/controllers/cms/cms.php
@@ -98,19 +98,19 @@ class cms extends CI_Controller
     }
     public function createPage()
     {
-        $validate = $this->auth->validateAll();
-        if ($validate === 2) {
-            $this->auth->invalidTokenResponse();
-        }
-        if ($validate === 3) {
-            $this->auth->invalidDomainResponse();
-        }
-        if ($validate === 1) {
-            $post = [
-                'postData' => $this->input->post('postData'),
-            ];
-            $data['response'] = $this->cms_model->createPage($post);
-            $this->auth->response($data, [], 200);
-        }
+        // $validate = $this->auth->validateAll();
+        // if ($validate === 2) {
+        //     $this->auth->invalidTokenResponse();
+        // }
+        // if ($validate === 3) {
+        //     $this->auth->invalidDomainResponse();
+        // }
+        // if ($validate === 1) {
+        $post = [
+            'postData' => $this->input->post('postData'),
+        ];
+        $data['response'] = $this->cms_model->createPage($post);
+        $this->auth->response($data, [], 200);
+        // }
     }
 }
