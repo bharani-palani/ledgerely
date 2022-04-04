1a4afab2342c698150cd187c4a14b76d30e87797
Bharani Palani <barani.potshot@gmail.com>
2022-04-05 00:13:06 +0530

    decode moved

--- services/application/controllers/cms/cms.php
+++ services/application/controllers/cms/cms.php
@@ -107,7 +107,6 @@ class cms extends CI_Controller
         }
         if ($validate === 1) {
             $post = $this->input->post('postData');
-            $post = json_decode($post);
             $data['response'] = $this->cms_model->createPage($post);
             $this->auth->response($data, [], 200);
         }
