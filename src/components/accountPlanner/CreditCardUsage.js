b9ed65632a21238b5b3efc83f9419b0de87b731d
bharani-palani <barani.potshot@gmail.com>
2022-10-02 18:30:40 +0530

    cc opening balance added to chart

--- src/components/accountPlanner/CreditCardUsage.js
+++ src/components/accountPlanner/CreditCardUsage.js
@@ -13,6 +13,7 @@ const CreditCardUsage = props => {
     const interestLineColor = getComputedStyle(document.documentElement).getPropertyValue('--bs-danger');;
     const purchasesLineColor = getComputedStyle(document.documentElement).getPropertyValue('--bs-success');
     const paidLineColor = getComputedStyle(document.documentElement).getPropertyValue('--bs-primary');
+    const openingLineColor = getComputedStyle(document.documentElement).getPropertyValue('--bs-warning');
 
     useEffect(() => {
         setWidth(ref.current.clientWidth)
@@ -43,6 +44,9 @@ const CreditCardUsage = props => {
     }, {
         color: paidLineColor,
         points: massageData('Paid')
+    }, {
+        color: openingLineColor,
+        points: massageData('Opening Balance')
     }];
 
     return (<div>
@@ -55,7 +59,7 @@ const CreditCardUsage = props => {
             <LineChart
                 margins={{ top: 50, right: 25, bottom: 50, left: 75 }}
                 width={width}
-                height={200}
+                height={300}
                 xLabel="Month"
                 yLabel="Transactions"
                 data={chartData}
@@ -67,9 +71,10 @@ const CreditCardUsage = props => {
                 onPointClick={(e, c) => onCcMonthYearSelected(c.month)}
             />
             <div className='row text-center p-1 m-2 rounded'>
-                <div className='col-md-4 small'><i className='fa fa-circle text-danger' /> {`Taxes & Interest ${getTotal('Taxes & Interest')}`}</div>
-                <div className='col-md-4 small'><i className='fa fa-circle text-success' /> {`Purchases ${getTotal('Purchases')}`}</div>
-                <div className='col-md-4 small'><i className='fa fa-circle text-primary' /> {`Paid ${getTotal('Paid')}`}</div>
+                <div className='col-md-3 small'><i className='fa fa-circle text-warning' /> Opening Blance</div>
+                <div className='col-md-3 small'><i className='fa fa-circle text-danger' /> {`Taxes & Interest ${getTotal('Taxes & Interest')}`}</div>
+                <div className='col-md-3 small'><i className='fa fa-circle text-success' /> {`Purchases ${getTotal('Purchases')}`}</div>
+                <div className='col-md-3 small'><i className='fa fa-circle text-primary' /> {`Paid ${getTotal('Paid')}`}</div>
             </div>
         </div>}
     </div>)
