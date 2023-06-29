5c87ddc1989c975eddb2b8fadd3aff09656da1d0
bharani-palani <tp.bharani@gmail.com>
2023-06-29 23:28:44 +0530

    icon redirect for mobile

--- src/components/mainApp/MobileApp.js
+++ src/components/mainApp/MobileApp.js
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
-import { Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import {SignedUrl} from '../configuration/Gallery/SignedUrl';
-import { UserContext } from '../../contexts/UserContext';
-import { FormattedMessage } from 'react-intl'
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { SignedUrl } from "../configuration/Gallery/SignedUrl";
+import { UserContext } from "../../contexts/UserContext";
+import { FormattedMessage } from "react-intl";
 
 const MobileApp = props => {
   const userContext = useContext(UserContext);
@@ -12,43 +12,46 @@ const MobileApp = props => {
   const menu = userContext.userData.menu;
 
   return (
-    <div className="mobile-menu">
+    <div className='mobile-menu'>
       <Navbar
-        style={{ top: '45px' }}
-        className={`py-0 ps-2 pe-3 ${userContext.userData.theme === 'dark' ? 'bg-dark' : 'bg-white'
-          }`}
-        fixed={'top'}
+        style={{ top: "45px" }}
+        className={`py-0 ps-2 pe-3 ${
+          userContext.userData.theme === "dark" ? "bg-dark" : "bg-white"
+        }`}
+        fixed={"top"}
         onToggle={onNavBarToggle}
         expanded={navBarExpanded}
-        expand="lg"
+        expand='lg'
       >
-        <Navbar.Brand className="navbar-brand pt-2">
-          <SignedUrl
-            type="image"
-            appData={appData}
-            unsignedUrl={appData.logoImg}
-            className="img-fluid"
-            optionalAttr={{ width: '40', height: '40' }}
-            mykey="logoImage"
-          />
+        <Navbar.Brand className='navbar-brand pt-2'>
+          <Link to='/'>
+            <SignedUrl
+              type='image'
+              appData={appData}
+              unsignedUrl={appData.logoImg}
+              className='img-fluid'
+              optionalAttr={{ width: "40", height: "40" }}
+              mykey='logoImage'
+            />
+          </Link>
         </Navbar.Brand>
         <Navbar.Toggle
-          style={{ boxShadow: 'none' }}
-          className="p-0 pe-1 fs-2 btn text-secondary border-1"
-          aria-controls="basic-navbar-nav"
-          bsPrefix="navbar-toggle"
+          style={{ boxShadow: "none" }}
+          className='p-0 pe-1 fs-2 btn text-secondary border-1'
+          aria-controls='basic-navbar-nav'
+          bsPrefix='navbar-toggle'
         >
           <i className={`fa fa-bars icon-bni`} />
         </Navbar.Toggle>
         <Navbar.Collapse>
-          <ul className="header-menu">
+          <ul className='header-menu'>
             {menu.map((m, i) => (
               <li key={i}>
                 <Link
                   className={
-                    userContext.userData.theme === 'dark'
-                      ? 'link-light'
-                      : 'link-dark'
+                    userContext.userData.theme === "dark"
+                      ? "link-light"
+                      : "link-dark"
                   }
                   onClick={onNavBarToggle}
                   to={m.href}
@@ -68,7 +71,7 @@ MobileApp.propTypes = {
   property: PropTypes.string,
 };
 MobileApp.defaultProps = {
-  property: 'String name',
+  property: "String name",
 };
 
 export default MobileApp;
