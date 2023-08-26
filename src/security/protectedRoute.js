3f7c67b10266615984d1d542b2b11a9c3b7b1e0e
bharani-palani <tp.bharani@gmail.com>
2023-08-26 17:42:02 +0530

    approuter removed from protected routes

--- src/security/protectedRoute.js
+++ src/security/protectedRoute.js
@@ -1,38 +1,31 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import AppContext from '../contexts/AppContext';
-import { UserContext } from '../contexts/UserContext';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
 
 export const ProtectedRoute = ({ component: Component, ...rest }) => {
   return (
-    <AppContext.Consumer>
-      {appData => {
+    <UserContext.Consumer>
+      {userInfo => {
         return (
-          <UserContext.Consumer>
-            {userInfo => {
-              return (
-                <Route
-                  {...rest}
-                  render={props => {
-                    if (rest.accessGiven.includes(userInfo.userData.type)) {
-                      return <Component {...props} />;
-                    } else {
-                      return (
-                        <Redirect
-                          to={{
-                            pathname: 'error',
-                            state: { from: props.location },
-                          }}
-                        />
-                      );
-                    }
-                  }}
-                />
-              );
+          <Route
+            {...rest}
+            render={props => {
+              if (rest.accessGiven.includes(userInfo.userData.type)) {
+                return <Component {...props} />;
+              } else {
+                return (
+                  <Redirect
+                    to={{
+                      pathname: "error",
+                      state: { from: props.location },
+                    }}
+                  />
+                );
+              }
             }}
-          </UserContext.Consumer>
+          />
         );
       }}
-    </AppContext.Consumer>
+    </UserContext.Consumer>
   );
 };
