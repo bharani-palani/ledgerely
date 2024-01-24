a879ed3f59dbf7579ba652448cbdaaf0c28c680e
bharani-palani <tp.bharani@gmail.com>
2024-01-24 18:38:47 +0530

    loader added

--- src/components/workbook/Canvas.js
+++ src/components/workbook/Canvas.js
@@ -26,63 +26,74 @@ const Canvas = props => {
     console.log("bbb", appendedComponents);
   }, [appendedComponents]);
 
+  const Loader = () => (
+    <div className='position-relative h-100'>
+      <div className='position-absolute w-100 h-100 d-flex align-items-center justify-content-center'>
+        <i className='fa fa-circle-o-notch fa-5x fa-spin icon-bni' />
+      </div>
+    </div>
+  );
+
   return (
-    <div className='position-relative'>
-      <div
-        style={{ zoom: zoom / 100 }}
-        className={`canvas canvas-${theme} w-100`}
-        onDrop={e => onDropHandle(e)}
-        onDragOver={e => {
-          e.preventDefault();
-        }}
-      >
-        <Suspense>
-          {appendedComponents.length &&
+    <Suspense fallback={<Loader />}>
+      <div className='position-relative'>
+        <div
+          style={{ zoom: zoom / 100 }}
+          className={`canvas canvas-${theme} w-100`}
+          onDrop={e => onDropHandle(e)}
+          onDragOver={e => {
+            e.preventDefault();
+          }}
+        >
+          {appendedComponents.length > 0 &&
             appendedComponents.map((a, i) => {
               const Component = chartList[a];
               return <Component key={i} />;
             })}
-        </Suspense>
+          <div className='position-absolute w-100 top-0 start-0'>
+            <Row>
+              <Col md={6}>
+                <InputGroup className='p-1' size='sm'>
+                  <Dropdown>
+                    <Dropdown.Toggle
+                      className={`bni-border bni-border-all bni-border-all-1 btn-bni`}
+                    >
+                      <FormattedMessage
+                        id='workbook'
+                        defaultMessage='workbook'
+                      />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu variant={theme}>
+                      <Dropdown.Item href='#'>Menu 1</Dropdown.Item>
+                      <Dropdown.Item href='#'>Menu 2</Dropdown.Item>
+                      <Dropdown.Item href='#'>Menu 3</Dropdown.Item>
+                      <Dropdown.Divider />
+                      <Dropdown.Item href='#'>Menu 4</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                  <Form.Control
+                    className='bni-border bni-border-all bni-border-all-1'
+                    placeholder={`${intl.formatMessage({
+                      id: "workbook",
+                      defaultMessage: "workbook",
+                    })} ${intl.formatMessage({
+                      id: "fileName",
+                      defaultMessage: "fileName",
+                    })}`}
+                  />
+                  <Button
+                    variant='outline-secondary'
+                    className='bni-border bni-border-all bni-border-all-1'
+                  >
+                    <i className='fa fa-save icon-bni' />
+                  </Button>
+                </InputGroup>
+              </Col>
+            </Row>
+          </div>
+        </div>
       </div>
-      <div className='position-absolute w-100 top-0 start-0'>
-        <Row>
-          <Col md={6}>
-            <InputGroup className='p-1' size='sm'>
-              <Dropdown>
-                <Dropdown.Toggle
-                  className={`bni-border bni-border-all bni-border-all-1 btn-bni`}
-                >
-                  <FormattedMessage id='workbook' defaultMessage='workbook' />
-                </Dropdown.Toggle>
-                <Dropdown.Menu variant={theme}>
-                  <Dropdown.Item href='#'>Menu 1</Dropdown.Item>
-                  <Dropdown.Item href='#'>Menu 2</Dropdown.Item>
-                  <Dropdown.Item href='#'>Menu 3</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href='#'>Menu 4</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-              <Form.Control
-                className='bni-border bni-border-all bni-border-all-1'
-                placeholder={`${intl.formatMessage({
-                  id: "workbook",
-                  defaultMessage: "workbook",
-                })} ${intl.formatMessage({
-                  id: "fileName",
-                  defaultMessage: "fileName",
-                })}`}
-              />
-              <Button
-                variant='outline-secondary'
-                className='bni-border bni-border-all bni-border-all-1'
-              >
-                <i className='fa fa-save icon-bni' />
-              </Button>
-            </InputGroup>
-          </Col>
-        </Row>
-      </div>
-    </div>
+    </Suspense>
   );
 };
 
