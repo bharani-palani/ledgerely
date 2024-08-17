48ce98f669548312bb7a3e24e306f215abb28e2c
bharani-palani <tp.bharani@gmail.com>
2024-08-18 00:11:55 +0530

    service worker build

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename={"/"}>
+    <BrowserRouter basename={"/"}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
+
+serviceWorker.unregister();
