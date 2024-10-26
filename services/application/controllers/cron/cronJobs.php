f4dec5562f29e7a94580fbf758e894c25f6ec76e
bharani-palani <tp.bharani@gmail.com>
2024-10-26 18:53:34 +0530

    Cron done

--- services/application/controllers/cron/cronJobs.php
+++ services/application/controllers/cron/cronJobs.php
@@ -31,27 +31,61 @@ class cronJobs extends CI_Controller
     public function quotaBatchUpdate()
     {
         try {
-
+            $batchSize = 10;
+            $apps = $this->home_model->getAllAppAccounts();
+            for ($i = 0; $i < count($apps); $i += $batchSize) {
+                $batch = array_slice($apps, $i, $batchSize);
+                $data = [];
+                foreach ($batch as $key => $item) {
+                    $banks = $this->home_model->getTableCount('banks', 'bank_appId', $item['appId']);
+                    $creditCards = $this->home_model->getTableCount('credit_cards', 'credit_card_appId', $item['appId']);
+                    $users = $this->home_model->getTableCount('users', 'user_appId', $item['appId']);
+                    $incomeExpenseTransactionSize = $this->home_model->getTableCount('income_expense', 'inc_exp_appId', $item['appId']);
+                    $creditCardTransactionSize = $this->home_model->getTableCount('credit_card_transactions', 'cc_appId', $item['appId']);
+                    $templateSize = $this->home_model->getTableCount('income_expense_template', 'temp_appId', $item['appId']);
+                    $categoriesSize = $this->home_model->getTableCount('income_expense_category', 'inc_exp_cat_appId', $item['appId']);
+                    $dataSourceSize = $this->home_model->getTableSize('datasourceQuery', 'dsq_object', 'dsq_appId', $item['appId']);
+                    $workbookSize = $this->home_model->getTableSize('workbook', 'wb_object', 'wb_appId', $item['appId']);
+                    $data[$key] = [
+                        'appId' => $item['appId'],
+                        'bankAccountsSize' => $banks,
+                        'creditCardsSize' => $creditCards,
+                        'usersSize' => $users,
+                        'incomeExpenseTransactionSize' => $incomeExpenseTransactionSize,
+                        'creditCardTransactionSize' => $creditCardTransactionSize,
+                        'storageSize' => 0,
+                        'templateSize' => $templateSize,
+                        'categoriesSize' => $categoriesSize,
+                        'dataSourceSize' => $dataSourceSize,
+                        'workbookSize' => $workbookSize,
+                    ];
+                }
+                $this->home_model->updateQuotaBatch('apps', $data, 'appId');
+                sleep(5);
+            }
             $config = $this->home_model->getGlobalConfig();
             $appName = $config[0]['appName'];
             $email = $config[0]['appSupportEmail'];
+            $adminEmail = $config[0]['appAdminEmail'];
 
-            $this->email->from($email, $appName . ' Support Team');
-            $this->email->to('tp.bharani@gmail.com');
-            $this->email->subject($appName . ' Your new password!');
+            $this->email->from($email, $appName . 'Automated Cron Job');
+            $this->email->to($adminEmail);
+            $this->email->subject($appName . ' - Cron job completed successfully!');
             $emailData['globalConfig'] = $config;
             $emailData['appName'] = $appName;
-            $emailData['saluation'] = 'Dear User,';
+            $emailData['saluation'] = 'Dear Admin,';
             $emailData['matter'] = [
-                'This is an auto generated cron mail',
-                'Please do not reply to this mail.'
+                'Total ' . count($apps) . ' app users have been updated with their quota usage.',
+                'Please note, this auto update process is only for active users.',
+                'This is an auto generated cron mail.',
             ];
             $emailData['signature'] = 'Regards,';
             $emailData['signatureCompany'] = $appName;
             $mesg = $this->load->view('emailTemplate', $emailData, true);
             $this->email->message($mesg);
-            $this->email->send();
-            $this->auth->response(['response' => 'Success!'], [], 200);
+            if ($this->email->send()) {
+                $this->auth->response(['response' => 'Success!'], [], 200);
+            }
         } catch (Exception $e) {
             $this->throwException($e);
         }
