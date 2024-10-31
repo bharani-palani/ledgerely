31556c47d5bfe898fd115f45b0c5767ba0b9f8d7
bharani-palani <tp.bharani@gmail.com>
2024-11-01 00:23:22 +0530

    home page style changed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -46,68 +46,65 @@ const Home = () => {
   }, [queryParamValue]);
 
   return (
-    <div className='container'>
-      <>
-        <div className='homeScreen'>
-          <Row className={`justify-content-md-center mainForm`}>
-            <Col lg={6} className={``}>
-              <img
-                src={Image}
-                alt='brand'
-                className='img-fluid helpImage w-100'
-              />
-            </Col>
-            <Col lg={6} className={`p-3 position-relative formWrapper`}>
-              <>
-                <LoginUser onLogAction={d => onLogAction(d)} />
-                <Row className='position-absolute bottom-0 text-dark small text-center w-100 align-items-center'>
-                  <Col sm={12} className=''>
-                    <Link
-                      to={"/signup"}
-                      className='btn btn-md btn-bni w-100 rounded-pill bg-gradient'
+    <div className=''>
+      <div className='bni-bg'>
+        <Row className={`vh-100 m-0 p-0 p-3`}>
+          <Col xxl={9} xl={9} lg={9} className='text-center py-2 p-0'>
+            <img src={Image} alt='brand' className='img-fluid rounded' />
+          </Col>
+          <Col
+            xxl={3}
+            xl={3}
+            lg={3}
+            className={`formArea p-0 bg-white rounded position-relative`}
+          >
+            <div className='p-2'>
+              <LoginUser onLogAction={d => onLogAction(d)} />
+              <div className='position-absolute bottom-0 w-100 pe-3 pb-2'>
+                <div className='pb-1'>
+                  <Link
+                    to={"/signup"}
+                    className='btn btn-sm btn-outline-primary w-100 rounded bg-gradient'
+                  >
+                    {intl.formatMessage({
+                      id: "signUp",
+                      defaultMessage: "signUp",
+                    })}
+                  </Link>
+                </div>
+                <div className='pb-1'>
+                  <a
+                    href={globalContext.privacyPolicyLink}
+                    target='_blank'
+                    className='btn btn-sm btn-primary w-100'
+                    rel='noreferrer'
+                  >
+                    {intl.formatMessage({
+                      id: "privacyPolicy",
+                      defaultMessage: "privacyPolicy",
+                    })}
+                  </a>
+                </div>
+                <div className='pb-1'>
+                  <div>
+                    <a
+                      href={globalContext.termsOfServiceLink}
+                      target='_blank'
+                      className='btn btn-sm btn-primary w-100'
+                      rel='noreferrer'
                     >
                       {intl.formatMessage({
-                        id: "signUp",
-                        defaultMessage: "signUp",
+                        id: "termsAndConditions",
+                        defaultMessage: "termsAndConditions",
                       })}
-                    </Link>
-                  </Col>
-                  <Col sm={6} className='p-2'>
-                    <div>
-                      <a
-                        href={globalContext.privacyPolicyLink}
-                        target='_blank'
-                        className='btn btn-sm btn-link'
-                        rel='noreferrer'
-                      >
-                        {intl.formatMessage({
-                          id: "privacyPolicy",
-                          defaultMessage: "privacyPolicy",
-                        })}
-                      </a>
-                    </div>
-                  </Col>
-                  <Col sm={6} className='p-2'>
-                    <div>
-                      <a
-                        href={globalContext.termsOfServiceLink}
-                        target='_blank'
-                        className='btn btn-sm btn-link'
-                        rel='noreferrer'
-                      >
-                        {intl.formatMessage({
-                          id: "termsAndConditions",
-                          defaultMessage: "termsAndConditions",
-                        })}
-                      </a>
-                    </div>
-                  </Col>
-                </Row>
-              </>
-            </Col>
-          </Row>
-        </div>
-      </>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
