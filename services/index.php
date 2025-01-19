98e1026ce2b4783c3c47a048dbe2e6f1a67f8349
bharani-palani <tp.bharani@gmail.com>
2025-01-19 17:18:44 +0530

    fix: develop spell error fix

--- services/index.php
+++ services/index.php
@@ -35,7 +35,7 @@ define('ENVIRONMENT', $_ENV['APP_ENV']);
 
 if (defined('ENVIRONMENT')) {
 	switch (ENVIRONMENT) {
-		case 'developm':
+		case 'develop':
 			error_reporting(E_ALL);
 			ini_set('display_errors', 1);
 			break;
