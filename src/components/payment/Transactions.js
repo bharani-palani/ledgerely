e15c163d1c33894406f55b9217798f62b901212a
bharani-palani <tp.bharani@gmail.com>
2025-02-08 15:32:00 +0530

    History lazy loading done

--- src/components/payment/Transactions.js
+++ src/components/payment/Transactions.js
@@ -1,35 +1,84 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import apiInstance from "../../services/apiServices";
 import { Table } from "react-bootstrap";
 import { UserContext } from "../../contexts/UserContext";
 import moment from "moment";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 
 const Transactions = () => {
+  const intl = useIntl();
   const userContext = useContext(UserContext);
+  const listInnerRef = useRef();
   const [data, setData] = useState([]);
+  const [params, setParams] = useState({
+    count: 10,
+    skip: 0,
+  });
+  const [loading, setLoading] = useState(false);
+  const [lazy, setLazy] = useState(true);
+
+  const onScroll = () => {
+    if (listInnerRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current;
+      const isNearBottom = scrollTop + clientHeight >= scrollHeight;
+      if (isNearBottom) {
+        setParams(prev => ({
+          ...prev,
+          skip: prev.skip + 10,
+        }));
+      }
+    }
+  };
 
   useEffect(() => {
-    const params = {
-      count: 10,
-      skip: 0,
-    };
+    const listInnerElement = listInnerRef.current;
+    if (listInnerElement) {
+      listInnerElement.addEventListener("scroll", onScroll);
+
+      return () => {
+        listInnerElement.removeEventListener("scroll", onScroll);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
     const query = new URLSearchParams(params);
     const queryString = query.toString();
-
-    apiInstance
-      .get(`/payments/razorpay/getTransactions?${queryString}`)
-      .then(res => {
-        console.log("bbb", res.data.response);
-        setData(res.data.response);
-      });
-  }, []);
+    if (lazy) {
+      setLoading(true);
+      apiInstance
+        .get(`/payments/razorpay/getTransactions?${queryString}`)
+        .then(res => {
+          setData(prev => ({
+            ...prev,
+            items:
+              params.skip === 0
+                ? res.data.response.items
+                : [...prev.items, ...res.data.response.items],
+          }));
+          setLazy(res?.data?.response?.count > 0);
+        })
+        .catch(err => {
+          console.error(err);
+          userContext.renderToast({
+            type: "error",
+            icon: "fa fa-times-circle",
+            message: intl.formatMessage({
+              id: "unableToReachServer",
+              defaultMessage: "unableToReachServer",
+            }),
+          });
+          setLazy(false);
+        })
+        .finally(() => setLoading(false));
+    }
+  }, [params]);
 
   const badgeStatus = {
     created: "bg-info",
     active: "bg-success",
     cancelled: "bg-danger",
-    completed: "bg-success",
+    completed: "bg-primary",
     authenticated: "bg-success",
     pending: "bg-warning",
     expired: "bg-danger",
@@ -37,21 +86,28 @@ const Transactions = () => {
     paused: "bg-warning",
   };
 
-  return data?.items && data?.items.length > 0 ? (
+  return (
     <div className='container-fluid'>
       <div className='fs-3 py-2'>
         <FormattedMessage id={"history"} defaultMessage={"history"} />
       </div>
-      <div className='table-responsive-sm'>
+      <div
+        className='table-responsive-sm'
+        style={{ maxHeight: "215px", overflow: "auto" }}
+        ref={listInnerRef}
+      >
         <Table
           striped
           bordered
           hover
           size='sm'
           variant={userContext.userData.theme}
+          className='table-mobile'
         >
-          <thead>
-            <tr>
+          <thead className='sticky-top top-0'>
+            <tr
+              className={`border border-1 ${userContext.userData.theme === "dark" ? "border-secondary" : ""}`}
+            >
               <th>
                 <FormattedMessage
                   id={"subscriptionId"}
@@ -84,48 +140,75 @@ const Transactions = () => {
               </th>
             </tr>
           </thead>
-          <tbody>
-            {data?.items?.map((item, index) => (
-              <tr key={index}>
-                <td>{item.id}</td>
-                <td>
-                  <span
-                    className={`badge ${badgeStatus[item.status]} text-capitalize`}
-                  >
-                    {item.status}
-                  </span>
-                </td>
-                <td>
-                  {moment
-                    .unix(item.current_start)
-                    .format("MMM Do YYYY, h:mm a")}
-                </td>
-                <td>
-                  {moment.unix(item.current_end).format("MMM Do YYYY, h:mm a")}
-                </td>
-                <td>
-                  {moment.unix(item.end_at).format("MMM Do YYYY, h:mm a")}
-                </td>
-                <td>
-                  {moment.unix(item.created_at).format("MMM Do YYYY, h:mm a")}
-                </td>
-                <td className='text-center'>
-                  <a
-                    target='_blank'
-                    rel='noreferrer'
-                    href={item.short_url}
-                    className='btn btn-sm btn-primary rounded-circle'
-                  >
-                    <i className='fa fa-link' />
-                  </a>
+          <tbody className='small'>
+            {data?.items && data?.items.length > 0 ? (
+              data?.items?.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.id}</td>
+                  <td>
+                    <span
+                      className={`badge ${badgeStatus[item.status]} text-capitalize`}
+                    >
+                      {item.status}
+                    </span>
+                  </td>
+                  <td>
+                    {moment
+                      .unix(item.current_start)
+                      .format("MMM Do YYYY, h:mm a")}
+                  </td>
+                  <td>
+                    {moment
+                      .unix(item.current_end)
+                      .format("MMM Do YYYY, h:mm a")}
+                  </td>
+                  <td>
+                    {moment.unix(item.end_at).format("MMM Do YYYY, h:mm a")}
+                  </td>
+                  <td>
+                    {moment.unix(item.created_at).format("MMM Do YYYY, h:mm a")}
+                  </td>
+                  <td className='text-center'>
+                    <a
+                      target='_blank'
+                      rel='noreferrer'
+                      href={item.short_url}
+                      className='btn-link'
+                    >
+                      <i className='fa fa-link' />
+                    </a>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={7} className='text-center'>
+                  <FormattedMessage
+                    id='noRecordsGenerated'
+                    defaultMessage='noRecordsGenerated'
+                  />
                 </td>
               </tr>
-            ))}
+            )}
           </tbody>
         </Table>
       </div>
+      <div className='py-1 small d-flex align-items-center justify-content-end'>
+        {data?.items?.length > 0 && (
+          <div>
+            <span>{Number(data?.items?.length)}</span>
+            <span className='px-1'>
+              <FormattedMessage
+                id={"recordsFound"}
+                defaultMessage={"recordsFound"}
+              />
+            </span>
+          </div>
+        )}
+        {loading && <i className='fa fa-circle-o-notch fa-spin' />}
+      </div>
     </div>
-  ) : null;
+  );
 };
 
 export default Transactions;
