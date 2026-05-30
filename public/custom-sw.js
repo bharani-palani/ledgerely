1363d8314f83cb7b6855c5f9d9386705d9f0c359
Bharani <tp.bharani@gmail.com>
2026-05-30 20:06:59 +0530

    Revert "overrides"
    This reverts commit d0fbeb110dbe940bc391f71deb557b7949acd490.

--- public/custom-sw.js
+++
@@ -1,9 +0,0 @@
-// public/custom-sw.js
-self.addEventListener("message", event => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-// Precache manifest will be injected here
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 // Register custom service worker for PWA
-// Register Workbox service worker for PWA
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
-  });
-}
+import * as serviceWorkerRegistration from "./serviceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,3 +10,5 @@ root.render(
     <App />
   </React.StrictMode>,
 );
+
+serviceWorkerRegistration.register();
