bef080ec61fe6b0a0fc208c1eeb8304579897a2c
Bharani <tp.bharani@gmail.com>
2026-06-04 19:05:48 +0530

    fix: base vit config

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,46 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  root: path.resolve(__dirname),
-  publicDir: "public",
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern",
-      },
-      sass: {
-        api: "modern",
-      },
-    },
-  },
-  optimize: {
-    esbuild: {
-      exclude: ["react-linechart"],
-    },
-  },
-  build: {
-    outDir: path.resolve(__dirname, "build"),
-    emptyOutDir: true,
-    sourcemap: "hidden",
-  },
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      "/services": {
-        target: "http://localhost:5001",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.VITE_SUBFOLDER ? `/${env.VITE_SUBFOLDER}/` : "/",
+    plugins: [react()],
+    root: path.resolve(__dirname),
+    publicDir: "public",
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern",
+        },
+        sass: {
+          api: "modern",
+        },
       },
     },
-  },
-  define: {
-    "process.env": JSON.stringify(process.env),
-  },
+    optimize: {
+      esbuild: {
+        exclude: ["react-linechart"],
+      },
+    },
+    build: {
+      outDir: path.resolve(__dirname, "build"),
+      emptyOutDir: true,
+      sourcemap: "hidden",
+    },
+    server: {
+      port: 3000,
+      open: true,
+      proxy: {
+        "/services": {
+          target: "http://localhost:5001",
+          changeOrigin: true,
+        },
+      },
+    },
+    define: {
+      "process.env": JSON.stringify(process.env),
+    },
+  };
 });
