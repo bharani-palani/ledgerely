1aba455b434927e15c1fe177a18efa3af3e92827
Bharani <tp.bharani@gmail.com>
2026-07-13 00:00:43 +0530

    fix: emoji + PWA UI mobile view fix

--- src/components/shared/D3/SmileyEmoji.jsx
+++ src/components/shared/D3/SmileyEmoji.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { smileyEmojiProps } from "./propsData";
 
 const SmileyEmoji = props => {
-  const { fontSize, emoji, showAnimation, animationClass } = { ...smileyEmojiProps, props };
+  const { fontSize, emoji, showAnimation, animationClass } = { ...smileyEmojiProps, ...props };
   return (
     <div
       className={`${showAnimation ? animationClass : ""} shape`}
