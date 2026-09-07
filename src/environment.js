f776b1bbe65aac605d8efde5964c083d43d5871f
Bharani <tp.bharani@gmail.com>
2026-09-07 19:29:44 +0530

    fix: cap base url + icons + link refs +

--- src/environment.js
+++ src/environment.js
@@ -1,10 +1,12 @@
 const baseUrl = () => {
   if (import.meta.env.MODE === "capacitor") {
     /**
-     * Base URL for Capacitor mobile app to be changed to /app/services when deployed to production
+     * Important: Base URL setup for Capacitor mobile app
      * http://localhost:5001/ledgerely/services is not working after several attempts to work in localhost.
+     * Hence declared VITE_MOBILE_APP_BASE_URL in .env file and used it here.
+     * This is working fine in both Android and IOS mobile apps.
      */
-    return "https://ledgerely.com/app/services";
+    return import.meta.env.VITE_MOBILE_APP_BASE_URL;
   }
   return import.meta.env.VITE_BASE_URL;
 };
